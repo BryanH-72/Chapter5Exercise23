cae792b3fb827fc8082e35463e04c4e889b3cfe8
BryanH <Bryan_c_hurley@outlook.com>
2025-04-20 22:33:17 -0400

    Fixed Header

--- Chapter5Exercise23.cpp
+++ Chapter5Exercise23.cpp
@@ -1,4 +1,4 @@
-/*File Name : Chap5Exercise23.cpp
+/*File Name : Chapter5Exercise23.cpp
  Developer/Programmer: Bryan Hurley
  Date: April 2025
  Requirements:
@@ -6,26 +6,6 @@
 First, it prints an increasing triangle of stars from 1 to 10.
 Then, it prints a decreasing triangle of stars from 10 to 1.
 The program repeats as long as the user wants. All input is validated.*/ 
-#include <iostream>
-#include <random>
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 #include <iostream>
 
 using namespace std;
@@ -69,6 +49,6 @@ int main() {
         displayDescending();
     } while (askRepeat());
 
-    cout << "Program ended\n";
+    cout << "Program ended. Goodbye!\n";
     return 0;
 }
